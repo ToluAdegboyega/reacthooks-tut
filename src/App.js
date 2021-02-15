c8448197f43228d6c17d435ed5e3b00c5e3a5d39
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-02-15 18:46:03 +0100

    chore: useState.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterFour from './components/HookCounterFour';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+
+     <HookCounterFour />
+
+     {/* 
+     <HookCounterThree />
+
+     <HookCounterTwo />
+
+     <HookCounter />
+
+     <ClassCounter /> 
+     */}
+    </div>
+  );
+}
+
+export default App;
