a2cc76dc90a404a13592dff4df6d9363795d623d
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-02-16 22:08:43 +0100

    chore: useEffect.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React from 'react';
 
 import './App.css';
 import ClassCounter from './components/ClassCounter';
+import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
+import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
 
-     <HookCounterFour />
+     <MouseContainer/>
 
      {/* 
+     <HookMouse />
+
+     <ClassMouse />
+
+     <HookCounterOne/>
+     <ClassCounterOne/>
+
+     <HookCounterFour />
+
      <HookCounterThree />
 
      <HookCounterTwo />
